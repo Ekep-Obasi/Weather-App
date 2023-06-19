09c7e75e7fe88681e755ae0cb16baa78315ac115
Ekep Obasi <arreyettaekep@gmail.com>
2023-06-19 02:30:14 +0100

    Deleted src/assets folder

--- src/components/atoms/WeatherIcon.jsx
+++ src/components/atoms/WeatherIcon.jsx
@@ -1,5 +1,10 @@
 const WeatherIcon = (props) => {
-  return <img src={props.image} alt="icons" />;
+  return (
+    <img
+      src={` http://openweathermap.org/img/w/${props.image}.png`}
+      alt="icons"
+    />
+  );
 };
 
 export default WeatherIcon;
