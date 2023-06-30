bb37e2aefa9115d2094635873539a82b10ce4d00
Ekep Obasi <arreyettaekep@gmail.com>
2023-06-30 13:59:10 +0100

    Built initial loading page

--- src/pages/LoadingPage/LoadingPage.styles.js
+++ src/pages/LoadingPage/LoadingPage.styles.js
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
-const LoadingPage = styled.div`
+const StyledLoadingPage = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
   width: 100%;
   background-color: ${(props) => props.theme.colors.darkBgPrimary};
+  color: white;
+  font-family: "Courgette", cursive;
+  font-size: 1.5rem;
+  color: #4fa94d;
+  gap: 15px;
+
+  .icon,
+  .weather {
+    color: #fdb813;
+  }
+  p {
+    color: white;
+  }
 `;
 
-export { LoadingPage };
+export { StyledLoadingPage };
--- src/pages/LoadingPage/LoadingPages.jsx
+++ src/pages/LoadingPage/LoadingPages.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { MutatingDots } from "react-loader-spinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { StyledLoadingPage } from "./LoadingPage.styles";
+import TypeEffect from "../../components/atoms/Loader/typeEffect";
 
-const LoadingPages = () => {
+const Spinner = () => {
   return (
-    <LoadingPages>
-      
-    </LoadingPages>
-  )
-}
+    <MutatingDots
+      height="100"
+      width="100"
+      color="#fdb813"
+      secondaryColor="#4fa94d"
+      radius="12.5"
+      ariaLabel="mutating-dots-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+};
 
-export default LoadingPages
+const LoadingPage = () => {
+  return (
+    <StyledLoadingPage>
+      <h1>
+        <span className="icon">
+          <FontAwesomeIcon
+            icon="fa-solid fa-sun"
+            beat
+            style={{ color: "#fdb813" }}
+          />
+        </span>
+        <span className="weather">Weather</span> Forecast
+      </h1>
+      <Spinner />
+      <p>
+        <TypeEffect string={["Please wait a minute..."]} />
+      </p>
+    </StyledLoadingPage>
+  );
+};
+
+export default LoadingPage;
